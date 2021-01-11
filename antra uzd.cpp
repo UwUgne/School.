e4f2b39fb602e2fcf8d382c21c8d19ae43d78725
Ugnė Klimavičiūtė <76807334+UwUgne@users.noreply.github.com>
2021-01-11 10:02:55 +0200

    Add files via upload

---
+++ antra uzd.cpp
@@ -0,0 +1,17 @@
+//antra uzduotis
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int long a;
+    int long men, d, v;
+    std::cout << "iveskite a reiksme";
+    std::cin >> a;
+    d = 365 * a;
+    v = d * 24;
+    men = a * 12;
+
+    std::cout << v;
+    std::cout << m;
+}
